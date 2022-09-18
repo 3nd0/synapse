2495a73c6289b569b0979e57a1269bd3731bc8db
wataru.endo <endo0430w@gmail.com>
2022-09-18 10:46:29 +0900

    refactor: Allow cors settings

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: '*',
+      allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+    },
+  });
   await app.listen(8080);
 }
 bootstrap();
